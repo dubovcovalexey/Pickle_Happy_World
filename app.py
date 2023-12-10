3a7397489ff00167e1db546aafe096e77b22e392
dubovcovalexey <134150801+dubovcovalexey@users.noreply.github.com>
2023-12-10 12:05:55 +0300

    app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def set_png_as_page_bg(png_file):
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
-set_png_as_page_bg('6.jpg')
+set_png_as_page_bg('phone.jpg')
 
 
 classifier_name=['Xgboost']
